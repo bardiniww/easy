2e60c200611b574d2f7543ec30908336cbd6cc4b
bardiniww <bardiniww@gmail.com>
2023-01-08 19:55:27 +0800

    integration test structure added

---
+++ src/test/java/com/easy/app/integration/UserIT.java
@@ -0,0 +1,17 @@
+package com.easy.app.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(
+        locations = "classpath:application-it.properties"
+)
+public class UserIT {
+
+    @Test
+    void name() {
+
+    }
+}
